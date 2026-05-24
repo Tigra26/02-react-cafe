a1af8d356ee2d63143bdb7c4d1184e7cf487eafa
Alena <shining8smaragd@gmail.com>
2026-05-24 08:52:17 +0300

    Final settings for notification and reset button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import type { Votes, VoteType } from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 
 
 function App() {
@@ -29,12 +30,20 @@ function resetVotes() {
     });
   };
 
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
   return (<div className={css['app']}>
     <CafeInfo  />
     
-    <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+    <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
 
-    <VoteStats votes={votes} totalVotes={0} positiveRate={0} />
+    {totalVotes > 0 && (<VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} />)}
+
+    {totalVotes === 0 && (<Notification />)}
   </div>);
 }
 
